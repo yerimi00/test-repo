dc461358907b03717f1495cf44cd1acfc24f7ac1
YerimLee <129266668+yerimi00@users.noreply.github.com>
2024-05-15 10:28:22 +0900

    feat : 스크롤바 없애기

--- Menu.js
+++ Menu.js
@@ -1,2 +1,9 @@
 const menuItem = food9;
-const menuItems = [{ food10 }, { food11 }, { food12 }, { food13 }, { food14 }];
+const menuItems = [
+  { food10 },
+  { food11 },
+  { food12 },
+  { food13 },
+  { food14 },
+  { food15 },
+];
